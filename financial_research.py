c02bf553d90b66425c4a81f8703246d0c244e5d0
MauriceLee <ee.maurice.lee@gmail.com>
2019-03-16 18:03:48 +0800

    add profit

--- financial_research.py
+++ financial_research.py
@@ -1,15 +1,72 @@
+import pandas as pd
+
+
+def read_csv(file):
+    df = pd.read_csv(file, converters={'Adj Close': float}) 
+    df = df['Adj Close']
+    return df.tolist()
+
+
 def three_days(data):
-    info = []
-    for i in range(len(data)):
-        if i < 3:
-            info.append(0)
-        elif data[i] > data[i-1] > data[i-2] > data[i-3]:
-            info.append(1)
-        elif data[i] < data[i-1] < data[i-2] < data[i-3]:
-            info.append(-1)
+    last = data[0]
+    big = 0
+    small = 0
+    result = []
+
+    for d in data:
+        if d > last:
+            small = 0
+            big += 1
+            if big >= 3:
+                result.append(1)
+            else:
+                result.append(0) 
+        elif d < last:
+            big = 0
+            small -= 1
+            if small <= -3:
+                result.append(-1)
+            else:
+                result.append(0)
         else:
-            info.append(0)
-    return info
-    
-data = [9422, 9468, 9512, 9524, 9550, 9450, 9410, 9368]
-print(three_days(data))
+            big = 0
+            small = 0
+            result.append(0)
+        last = d
+
+    return result
+
+
+def calc_profit(data, signal):
+    pos = 0
+    entry = 0
+    total  = 0
+    for i in range(len(data)):
+        if signal[i] == 1:
+            if pos == 0:
+                entry = data[i]
+            elif pos == -1:
+                profit = entry - data[i]
+                total += profit
+                entry = data[i]
+            pos = 1
+        elif signal[i] == -1:
+            if pos == 0:
+                entry = data[i]
+            elif pos == 1:
+                profit = data[i] - entry
+                total += profit
+                entry = data[i]
+            pos = -1
+
+    return total * 1000
+
+
+def main():
+    data = read_csv('2330.csv')
+    signal = three_days(data)
+    total = calc_profit(data, signal)
+    print(total)
+
+
+main()
